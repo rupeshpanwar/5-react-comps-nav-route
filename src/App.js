fc45a9bb18c45c5792842824ed7dc724eff4cf56
Jon Doe <jondoe@doe.com>
2023-02-19 17:10:04 +0800

    step2-button validation-props- adding prop-types to validate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
-
+import Button from './Button';
 
 function App() {
   return (
-    <div >
-      App
+    <div>
+      <div>
+        <Button primary rounded outline>
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Something!
+        </Button>
+      </div>
     </div>
   );
 }

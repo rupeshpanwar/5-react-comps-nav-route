ea71463d643e1a5102d9c9ea2f12bc6124e014cd
Jon Doe <jondoe@doe.com>
2023-02-19 19:25:34 +0800

    step4- bit more to styling

--- src/Button.js
+++ src/Button.js
@@ -17,6 +17,13 @@ function Button({
     'border-green-500 bg-green-500 text-white': success,
     'border-yellow-500 bg-yellow-500 text-white': warning,
     'border-red-500 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white':outline,
+    'text-blue-500':outline && primary,
+    'text-gray-500': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-500': outline && warning,
+    'text-red-500': outline && danger,
   })
   return (
     <button className={classes}>

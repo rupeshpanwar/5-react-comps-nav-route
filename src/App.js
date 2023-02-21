e043ef552345ef863004eda237db169cdcae4d11
rupesh.panwar <rupeshpanwar28@gmail.com>
2023-02-21 16:00:52 +0800

    step2-add list and display as per index

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './Button';
+import Accordion from './components/Accordion';
 
 function App() {
+  const items = [
+    {
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      label: 'Can I use Javascript on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+    {
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
+    },
+  ];
 
-  const handleClick = () => {
-      console.log('Click!')
-  }
-
-  return (
-    <div>
-      <div>
-        <Button secondary outline rounded className="mb-5" onClick={handleClick}>
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Accordion({ items }) {
+  
+  const [expandedIndex,setExpandedIndex] = useState(0)
+
+
+  const renderedItems = items.map((item, index) => {
+
+    const isExpanded = index === expandedIndex
+
+     
+
+  return (
+  <div key={index}>
+          <div>{item.label}</div>
+          {isExpanded && <div>{item.content}</div>}
+  </div>
+  );
+  });
+  
+  return <div>{renderedItems}</div>;
+  }
+  
+  export default Accordion;

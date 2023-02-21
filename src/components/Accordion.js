4ddb8dfa3374bd5cfc3806004d9eac8e4c1bf15e
Jon Doe <jondoe@doe.com>
2023-02-22 07:33:16 +0800

    step4- styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,25 +1,34 @@
 import { useState } from "react";
-
+import {GoChevronDown, GoChevronLeft}  from "react-icons/go";
 function Accordion({ items }) {
   
   const [expandedIndex,setExpandedIndex] = useState(0)
 
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex)
+  }
 
   const renderedItems = items.map((item, index) => {
 
     const isExpanded = index === expandedIndex
 
-     
+    const icon = (
+      <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    ) 
 
   return (
   <div key={index}>
-          <div>{item.label}</div>
-          {isExpanded && <div>{item.content}</div>}
+          <div className="flex justify-center p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=> handleClick
+          (index)}>{item.label}
+           {icon}
+          </div>
+         
+          {isExpanded && <div className="border-b p-5">{item.content}</div>}
   </div>
   );
   });
   
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
   }
   
   export default Accordion;

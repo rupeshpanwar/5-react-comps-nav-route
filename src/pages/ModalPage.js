465c7cdf354cb600c4bd5824578efe7e699cb869
Jon Doe <jondoe@doe.com>
2023-02-26 18:57:42 +0800

    modal-step2-add basic Modal design, reuse button

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,19 @@
 import Modal from "../components/Modal"
+import Button from "../components/Button"
+import { useState } from "react"
 
 function ModalPage(){
+
+    const [showModal,setShowModal] = useState(false)
+
+    const handleClick = () => {
+        setShowModal(true)
+    }
+
     return (
         <div>
-            <Modal />
+            <Button onClick={handleClick} >Open Modal</Button>
+            {showModal && <Modal />}
         </div>
     )
 }

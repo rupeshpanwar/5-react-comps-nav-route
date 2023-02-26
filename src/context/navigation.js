9bfe2e218e07d8e738a1b5a9e36cf6e04a17f0bb
Jon Doe <jondoe@doe.com>
2023-02-26 11:03:52 +0800

    step1-routing-useEffect to add currentPath

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,10 +1,26 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect  } from 'react';
 
 // Creating the context
 const NavigationContext = createContext();
 
 // Provider component to wrap around the app and provide the context value
 function NavigationProvider({children }) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        } 
+
+        window.addEventListener('popstate', handler)
+
+        return () => {
+            window.removeEventListener("popstate", handler);
+          };
+     
+    }, []);
+
+
     return(
         <NavigationContext.Provider value={{}}>{children}</NavigationContext.Provider>
     )

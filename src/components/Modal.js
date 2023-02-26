e0a2c7f90c01eb83f378918b5b31f1fb27007242
Jon Doe <jondoe@doe.com>
2023-02-26 19:52:13 +0800

    modal-step3-use createPort to design modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,15 @@
+import { createPortal } from 'react-dom';
+
 function Modal(){
-    return <div>Modal!</div>
+return createPortal(
+                <div>
+                        <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+                        <div className="absolute inset-40 p-10 bg-white">
+                        I'm a Modal
+                        </div>
+                </div>,
+            document.querySelector('.modal-container')
+);
 }
 
 export default Modal;

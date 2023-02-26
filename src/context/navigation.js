9e3a7cc2e2733e566f5262c6a82e532887b25faf
Jon Doe <jondoe@doe.com>
2023-02-26 11:17:17 +0800

    step3-routing-add Navigate to route to To

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,34 +1,34 @@
-import { createContext, useState, useEffect  } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-// Creating the context
 const NavigationContext = createContext();
 
-// Provider component to wrap around the app and provide the context value
-function NavigationProvider({children }) {
-    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-    useEffect(() => {
-        const handler = () => {
-            setCurrentPath(window.location.pathname);
-        } 
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', handler);
 
-        window.addEventListener('popstate', handler)
+    return () => {
+      window.removeEventListener('popstate', handler);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener("popstate", handler);
-          };
-     
-    }, []);
+  const navigate = (to) => {
+    window.history.pushState({}, '', to);
+    setCurrentPath(to);
+  };
 
-
-    return(
-        <NavigationContext.Provider value={{}}>{children}</NavigationContext.Provider>
-    )
-
+  return (
+    <NavigationContext.Provider value={{ currentPath, navigate }}>
+      {children}
+    </NavigationContext.Provider>
+  );
 }
 
-// Consumer component to access the context value from within a component
-
+export { NavigationProvider };
 export default NavigationContext;
 
 
